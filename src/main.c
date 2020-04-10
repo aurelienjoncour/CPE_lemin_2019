23c80c9eb13e15425d9431efd78176b687296471
Aurelien Joncour <aurelien.joncour@epitech.eu>
2020-04-10 19:27:39 +0200

    fix(src/main.c) changing order of function call (remove segfault)

--- src/main.c
+++ src/main.c
@@ -32,13 +32,20 @@ void display_room(room_t *room)
 int main(void)
 {
     char ***array_3d = get_script(stdin);
-    room_t **rooms_array = create_rooms(array_3d);
-    lemin_t *lemin = get_lemin_info(array_3d, rooms_array);
+    room_t **rooms_array = NULL;
+    lemin_t *lemin = NULL;
     int ret = 0;
 
-    if (array_3d == NULL || rooms_array == NULL || lemin == NULL)
+    if (array_3d == NULL)
         return 84;
-    ret = display_info(array_3d);
+    if (display_info(array_3d) == 84) {
+        free_3d_array(array_3d);
+        return 84;
+    }
+    rooms_array = create_rooms(array_3d);
+    lemin = get_lemin_info(array_3d, rooms_array);
     free_3d_array(array_3d);
+    if (rooms_array == NULL || lemin == NULL)
+        return 84;
     return ret;
 }
